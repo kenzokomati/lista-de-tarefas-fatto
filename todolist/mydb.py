c869feb5fda69c04824dcbe083860be9a230c518
Erick Komati <109813173+kenzokomati@users.noreply.github.com>
2026-01-24 15:04:04 -0300

    Revert "."
    This reverts commit be9c285cb2290d113a8f8b36773ced93009d00d5.

--- todolist/mydb.py
+++ todolist/mydb.py
@@ -1,13 +1,13 @@
-# import mysql.connector
+import mysql.connector
 
-# database = mysql.connector.connect(
-#   host='localhost',
-#   user='root',
-#   passwd='Abrico123',
-# )
+database = mysql.connector.connect(
+  host='localhost',
+  user='root',
+  passwd='Abrico123',
+)
 
-# cursorObject = database.cursor()
+cursorObject = database.cursor()
 
-# cursorObject.execute('CREATE DATABASE testeTecnico')
+cursorObject.execute('CREATE DATABASE testeTecnico')
 
-# print('DB created')
+print('DB created')
--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -70,11 +70,11 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DB_NAME", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("DB_USER", ""),
-        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
-        "HOST": os.environ.get("DB_HOST", ""),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
